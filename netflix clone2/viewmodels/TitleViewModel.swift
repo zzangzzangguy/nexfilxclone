98c8d45d1b403cc41a96b8ab562fe8e992991d02
zangzangguy <creeps1104@gmail.com>
2023-02-14 01:47:39 +0900

    beakpoint

---
+++ netflix clone2/viewmodels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  netflix clone2
+//
+//  Created by 김기현 on 2023/02/13.
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
---
+++ netflix clone2/views/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.swift
+//  netflix clone2
+//
+//  Created by 김기현 on 2023/02/13.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
